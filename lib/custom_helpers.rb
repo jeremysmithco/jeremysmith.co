8864a751a7b45aa00a1902e2e7dbfc2b92e24b17
Jeremy Smith <jeremy@bentoncreation.com>
2017-02-26 09:13:16 -0500

    added pragprog and notes

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -1,2 +1,8 @@
 module CustomHelpers
+  def markdown(text)
+    return "" if text.blank?
+    renderer = Redcarpet::Render::HTML.new(autolink: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer)
+    markdown.render(text)
+  end
 end
